0c1571286e3f8b6abbc20a864a0d0ef0b2bd4c2c
kaique oliveira santos <kaiquevicio@gmail.com>
2021-05-26 20:36:22 -0300

    Domain model

--- backend/src/main/java/com/weeksuper/weeksuper/entities/Order.java
+++ backend/src/main/java/com/weeksuper/weeksuper/entities/Order.java
@@ -1,20 +1,27 @@
 package com.weeksuper.weeksuper.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String address;
     private Double latitude, longitude;
     private Instant moment;
     private OrderStatus status;
 
+    @ManyToMany
+    @JoinTable(name = "tb_order_product", joinColumns = @JoinColumn(name = "order_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
     private Set<Product> products = new HashSet<>();
 
     public Order() {}

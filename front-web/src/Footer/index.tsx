a7bcd331fbadfa1a3053a8dca16db6ca97564255
kaique oliveira santos <kaiquevicio@gmail.com>
2021-05-28 23:13:57 -0300

    Add Footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,6 +1,24 @@
+import './styles.css';
+import { ReactComponent as YoutubeIcon } from "./youtube.svg";
+import { ReactComponent as LinkedinIcon } from "./linkedin.svg";
+import { ReactComponent as InstagramIcon } from "./instagram.svg";
+
 function Footer() {
   return(
-    <h1>Footer</h1>
+    <footer className="main-footer">
+      App desenvolvido por Kaique de Oliveira Santos na semana Dev superior 2° ed.
+      <div className="footer-icons">
+        <a href="https://www.youtube.com/?hl=pt&gl=BR" target="_new">
+          <YoutubeIcon />
+        </a>
+        <a href="https://www.linkedin.com/in/kaique-oliveira-santos-75a3951a0/" target="_new">
+          <LinkedinIcon />
+        </a>
+        <a href="https://www.instagram.com/" target="_new">
+          <InstagramIcon />
+        </a>
+      </div>
+    </footer>
   );
 }
 

b86806155999c9511a653c061deff1f9b414c659
kaique oliveira santos <kaiquevicio@gmail.com>
2021-06-06 14:29:05 -0300

    Fix

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import Select from 'react-select';
